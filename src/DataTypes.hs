6eeed0ca28fd526aa9ad85e90b87edb901d53354
Pavel Ivanov <tyrension@gmail.com>
2018-10-28 14:26:54 +0700

    Finish Data types section

--- src/DataTypes.hs
+++ src/DataTypes.hs
@@ -5,6 +5,8 @@ module DataTypes where
 -- import System.Locale
 import Data.List (findIndex)
 import Data.Char (isDigit, isSpace)
+import Prelude hiding (lookup)
+import qualified Data.List as L
 
 -- 4.1
 
@@ -218,3 +220,125 @@ getPair s = case findIndex (== '=') s of
 trim :: String -> String
 trim = f . f where
     f = reverse . dropWhile isSpace
+
+eitherToMaybe :: Either a a -> Maybe a
+eitherToMaybe (Left a) = Just a
+eitherToMaybe (Right _) = Nothing
+
+-- 4.5
+
+data List a = Nil | Cons a (List a)
+
+fromList :: List a -> [a]
+fromList Nil = []
+fromList (Cons x xs) = x : fromList xs
+
+toList :: [a] -> List a
+toList [] = Nil
+toList (x:xs) = Cons x $ toList xs
+
+data Nat = Zero | Suc Nat deriving (Show)
+
+fromNat :: Nat -> Integer
+fromNat Zero = 0
+fromNat (Suc n) = fromNat n + 1
+
+add :: Nat -> Nat -> Nat
+add Zero Zero = Zero
+add x Zero = x
+add Zero x = x
+add (Suc n) x = add n (Suc x)
+
+mul :: Nat -> Nat -> Nat
+mul Zero _ = Zero
+mul _ Zero = Zero
+mul (Suc Zero) n = n
+mul n (Suc Zero) = n
+mul x y = helper x Zero y where
+    helper t acc Zero = acc
+    helper t acc (Suc s) = helper t (add acc t) s
+
+fac :: Nat -> Nat
+fac Zero = Suc Zero
+fac (Suc Zero) = Suc Zero
+fac n = helper n (Suc Zero) where
+    helper (Suc Zero) acc = acc
+    helper t@(Suc n) acc = helper n $ mul acc t
+
+--
+
+data Tree a = Leaf a | Node (Tree a) (Tree a)
+
+height :: Tree a -> Int
+height (Leaf x) = 0
+height (Node x y) = 1 + max (height x) (height y)
+
+size :: Tree a -> Int
+size (Leaf x) = 1
+size (Node x y) = 1 + size x + size y
+
+avg :: Tree Int -> Int
+avg t =
+    let (c, s) = go t
+    in s `div` c
+  where
+    go :: Tree Int -> (Int, Int)
+    go (Leaf x) = (1, x)
+    go (Node x y) = (c1 + c2, s1 + s2) where
+        (c1, s1) = go x
+        (c2, s2) = go y
+
+--
+
+newtype Xor = Xor { getXor :: Bool }
+    deriving (Eq, Show)
+
+instance Semigroup Xor where
+    mempty = Xor False
+    Xor x `mappend` Xor y = Xor $ x /= y
+
+-- newtype Maybe' a = Maybe' { getMaybe :: Maybe a }
+--     deriving (Eq, Show)
+
+-- instance Monoid a => Monoid (Maybe' a) where
+--     mempty = Maybe' $ Just mempty
+--     Maybe' Nothing `mappend` _ = Maybe' Nothing
+--     _ `mappend` Maybe' Nothing = Maybe' Nothing
+--     Maybe' m1 `mappend` Maybe' m2 = Maybe' $ m1 `mappend` m2
+
+-- class MapLike m where
+--     empty :: m k v
+--     lookup :: Ord k => k -> m k v -> Maybe v
+--     insert :: Ord k => k -> v -> m k v -> m k v
+--     delete :: Ord k => k -> m k v -> m k v
+--     fromList :: Ord k => [(k,v)] -> m k v
+--     fromList [] = empty
+--     fromList ((k, v):xs) = insert k v (fromList xs)
+
+-- newtype ListMap k v = ListMap { getListMap :: [(k,v)] }
+--     deriving (Eq, Show)
+
+-- instance MapLike ListMap where
+--     empty = ListMap []
+--     lookup k (ListMap m) = L.lookup k m
+--     insert k v mo@(ListMap m) = case lookup k mo of
+--         Nothing -> ListMap $ m ++ [(k, v)]
+--         Just _  -> ListMap $ insert' k v m  where
+--             insert' k v [] = []
+--             insert' k v ((k1, v1):xs) | k == k1 = (k, v) : xs
+--                                       | otherwise = (k1, v1) : insert' k v xs
+
+--     delete k (ListMap []) = ListMap []
+--     delete k (ListMap m) = ListMap $ delete' k m where
+--         delete' k [] = []
+--         delete' k ((k1, v1):xs) | k == k1 = xs
+--                               | otherwise = (k1, v1) : delete' k xs
+
+newtype ArrowMap k v = ArrowMap { getArrowMap :: k -> Maybe v }
+
+instance MapLike ArrowMap where
+    empty = ArrowMap $ \_ -> Nothing
+    lookup k (ArrowMap f) = f k
+    insert k v (ArrowMap f) = ArrowMap $ \k1 -> if k1 == k then Just v else (f k1)
+    delete k (ArrowMap f) = ArrowMap $ \k1 -> if k1 == k then Nothing else (f k1)
+        
